133c91741227b767a9082605607f4c639673bc7b
tsb4825 <tim.byars@live.com>
2018-06-29 19:48:47 -0500

    initial

---
+++ jeopardy.js
@@ -0,0 +1,56 @@
+var jeopardyRoundClues = [
+  {
+    "Category": "Movies & TV",
+    "Clues": ["","","","",""]
+  },
+  {
+    "Category": "Sports",
+    "Clues": ["","","","",""]
+  },
+  {
+    "Category": "Words and Phrases",
+    "Clues": ["","","","",""]
+  },
+  {
+    "Category": "Education",
+    "Clues": ["","","","",""]
+  },
+  {
+    "Category": "Music",
+    "Clues": ["","","","",""]
+  },
+  {
+    "Category": "Work",
+    "Clues": ["","","","",""]
+  }
+];
+
+var doubleJeopardyRoundClues = [
+  {
+    "Category": "Travel & Leisure",
+    "Clues": ["","","","",""]
+  },
+  {
+    "Category": "Technology",
+    "Clues": ["","","","",""]
+  },
+  {
+    "Category": "Running",
+    "Clues": ["","","","",""]
+  },
+  {
+    "Category": "Church & Religion",
+    "Clues": ["","","","",""]
+  },
+  {
+    "Category": "Family",
+    "Clues": ["","","","",""]
+  },
+  {
+    "Category": "Shopping",
+    "Clues": ["","","","",""]
+  }
+];
+
+var finalJeopardyCategory = "Entertainment";
+var finalJeopardyAnswer = "";
